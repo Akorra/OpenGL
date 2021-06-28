bde7b61931c88cc8abe4e45bba304a1c2762280d
Akorra <neverfearphilishere@gmail.com>
2021-06-28 05:07:52 +0100

    model view projection

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -51,10 +51,10 @@ int main(void)
     { //Scope to force delete of stack allocated buffers
     //Triangle x1, y1, x2, y2, x3, y3
         float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f, //0
-             0.5f, -0.5f, 1.0f, 0.0f, //1
-             0.5f,  0.5f, 1.0f, 1.0f, //2
-            -0.5f,  0.5f, 0.0f, 1.0f  //3
+             100.0f, 100.0f, 0.0f, 0.0f, //0
+             200.0f, 100.0f, 1.0f, 0.0f, //1
+             200.0f, 200.0f, 1.0f, 1.0f, //2
+             100.0f, 200.0f, 0.0f, 1.0f  //3
         };
 
         unsigned int indices[] = {
@@ -76,12 +76,16 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj  = glm::ortho(0.0f, 960.0f, 0.5f, 540.0f, -1.0f, 1.0f); 
+        glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        glm::mat4 mvp = proj * view * model; // opengl matrix multiplication is right to left
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/dickbutt.png");
         texture.Bind();
